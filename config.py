4f3ad200443a9b1cd4c9ade6514d4fb78910b19f
avinashoffl <avinashgithun@gmail.com>
2024-09-06 16:00:03 +0530

    Add files via upload

---
+++ config.py
@@ -0,0 +1,19 @@
+# Don't use quotes( " and ' )
+#SCRIPT BY VENOMxCRAZY
+  
+#Enter Your Bot Token here get it from @botfarher
+BOT_TOKEN=("7235368503:AAF5ld7gbMj6mG_WhqUBR0R45Nw-")
+
+  #Enter Your telegram username here without @
+OWNER_USERNAME=("username")
+
+  #Enter your admin id here Get it from @missRose_bot by typing /info
+ADMIN_IDS=("620779474")
+
+
+
+
+
+
+
+  
