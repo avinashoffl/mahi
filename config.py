6e064db77aeee88a3d86be96053d09fb70461bbb
avinashoffl <avinashgithun@gmail.com>
2024-09-06 16:13:10 +0530

    Update config.py

--- config.py
+++ config.py
@@ -5,7 +5,7 @@
 BOT_TOKEN=("7351147654:AAHVW3ntjnQodNept5PfPQIxka5EfV5_H7o-")
 
   #Enter Your telegram username here without @
-OWNER_USERNAME=("hoffmanmolecule")
+OWNER_USERNAME=("IRFAN_HACK")
 
   #Enter your admin id here Get it from @missRose_bot by typing /info
 ADMIN_IDS=("868480450")
